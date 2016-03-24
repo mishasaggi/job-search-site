fdc95963cae762f4a05cb325dab7fff60e9744cd
mishasaggi <monisha.saggi@gmail.com>
2016-03-23 20:35:58 -0500

    Set up initial folder structure and basic code.

---
+++ client/app/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('app', [])
+
+  .controller('SearchController', function($scope){
+    $scope.results = [];
+    $scope.userInput = {
+      jobTitle: "",
+      zipcode: ""
+    }
+
+    $scope.fetchJobs = function() {
+      console.log("control in fetch jobs now");
+      console.log("user-entered: ", $scope.userInput);
+
+      //http post
+    }
+
+  })
