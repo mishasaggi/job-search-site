39ffa48ede70ba0b39ec9219d29371adce706c72
mishasaggi <monisha.saggi@gmail.com>
2016-03-24 11:34:38 -0500

    Add indeed attribute. Closes #13.

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 
 var server = http.createServer(function(request, response){
 
+
   if(request.method === 'GET' && request.url === '/') {
     response.writeHead(200, {'Content-Type': 'text/html'});
     fs.createReadStream('index.html').pipe(response);
@@ -14,6 +15,14 @@ var server = http.createServer(function(request, response){
   if(request.method === 'GET' && request.url === '/client/app/app.js'){
     response.writeHead(200, {'Content-Type': 'application/javascript'});
     fs.createReadStream('client/app/app.js').pipe(response);
+  }
+  if(request.method === 'GET' && request.url === 'http://www.indeed.com/ads/apiresults.js'){
+    response.writeHead(200, {'Content-Type': 'application/javascript'});
+    fs.createReadStream('http://www.indeed.com/ads/apiresults.js').pipe(response);
+  }
+  if(request.method === 'GET' && request.url === 'http://www.indeed.com/p/jobsearch.gif'){
+    response.writeHead(200, {'Content-Type': 'image/gif'});
+    fs.createReadStream('http://www.indeed.com/ads/apiresults.js').pipe(response);
   }
 
 }).listen(8080);
