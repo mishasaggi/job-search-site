bddd8b6378aca4e8b7de13fead73bdafe734a772
mishasaggi <monisha.saggi@gmail.com>
2016-03-23 23:23:12 -0500

    Serve front-end from node server. Closes #1 and closes #2.

--- server/server.js
+++ server/server.js
@@ -1,35 +1,20 @@
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer(function(request, response){
-  var headers = request.headers;
-  var method = request.method;
-  var url = request.url;
-  var body = [];
-  request.on('error', function(err){
-    console.log(err);
-  }).on('data', function(chunk){
-    body.push(chunck);
-  }).on('end', function(){
-    body = Buffer.concat(body).toString();
 
-    response.on('error', function(){
-      console.log(err);
-    })
-
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'application/json');
-    // or  response.writeHead(200, {"Content-Type": "text/plain"});
-
-    var responseBody = {
-      headers: headers,
-      method: method,
-      url: url,
-      body: body
-    };
-
-    response.write(JSON.stringify(responseBody));
-    response.end();
-
-  });
+  if(request.method === 'GET' && request.url === '/') {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    fs.createReadStream('index.html').pipe(response);
+  }
+  if(request.method === 'GET' && request.url === '/node_modules/angular/angular.min.js'){
+    response.writeHead(200, {'Content-Type': 'application/javascript'});
+    fs.createReadStream('node_modules/angular/angular.min.js').pipe(response);
+  }
+  if(request.method === 'GET' && request.url === '/client/app/app.js'){
+    response.writeHead(200, {'Content-Type': 'application/javascript'});
+    fs.createReadStream('client/app/app.js').pipe(response);
+  }
 
 }).listen(8080);
+
