e579b850ff084523cd29ee207498c10c381d59ff
mishasaggi <monisha.saggi@gmail.com>
2016-03-23 21:39:16 -0500

    Add basic request handling to server.

--- server/server.js
+++ server/server.js
@@ -1,9 +1,35 @@
 var http = require('http');
 
 var server = http.createServer(function(request, response){
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
+  var headers = request.headers;
+  var method = request.method;
+  var url = request.url;
+  var body = [];
+  request.on('error', function(err){
+    console.log(err);
+  }).on('data', function(chunk){
+    body.push(chunck);
+  }).on('end', function(){
+    body = Buffer.concat(body).toString();
 
+    response.on('error', function(){
+      console.log(err);
+    })
+
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'application/json');
+    // or  response.writeHead(200, {"Content-Type": "text/plain"});
+
+    var responseBody = {
+      headers: headers,
+      method: method,
+      url: url,
+      body: body
+    };
+
+    response.write(JSON.stringify(responseBody));
+    response.end();
+
+  });
 
 }).listen(8080);
