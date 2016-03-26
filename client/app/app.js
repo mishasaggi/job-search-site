6c56db4c42bc94be8d4a8251182fd5ba3c1d2244
mishasaggi <monisha.saggi@gmail.com>
2016-03-25 22:44:54 -0500

    Build client side search component.

--- client/app/app.js
+++ client/app/app.js
@@ -1,2 +1,19 @@
 // inject all dependencies
+angular.module('app', [
+  'ngRoute',
+  'app.search'
+  ])
+
 // routing
+  .config(["routeProvider", "$httpProvider", function($routeProvider, $httpProvider){
+    $routeProvider
+      .when('/', {
+        templateUrl: 'index.html'
+      })
+      .when('/search', {
+        templateUrl: 'app/search/search.html',
+        controller: 'SearchController'
+      })
+      .otherwise('/');
+
+  }])
--- client/app/search/searchController.js
+++ client/app/search/searchController.js
@@ -1,2 +1,24 @@
-// angular controller
-// call service(factory methods) to post user inputs to the server
+angular.module('app.search', [])
+  .controller('SearchController', ["$scope", "UserQuery", function($scope, UserQuery) {
+    $scope.userInput = {
+      jobTitle: "",
+      zipcode: 0,
+    }
+    $scope.userInputError = false;
+
+    $scope.searchNow = function(){
+      //validation
+      if($scope.userInput.jobTitle === undefined || $scope.userInput.zipcode === undefined) {
+        $scope.userInputError = true;
+      } else {
+        $scope.userInputError = false;
+        //call the service method
+        UserQuery.getJobs($scope.userInput)
+          //change view to render results
+          .then(function(){
+            $location.path("/search");
+          })
+      }
+    }
+
+  }]);
