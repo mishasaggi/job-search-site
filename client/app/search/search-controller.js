f705aa345c109adb10b9f8946a8dd0d21b037158
mishasaggi <monisha.saggi@gmail.com>
2016-03-29 11:08:32 -0500

    Refactor search views and controller.

--- client/app/search/search-controller.js
+++ client/app/search/search-controller.js
@@ -1,9 +1,10 @@
-console.log("in the controller");
+console.log("in the search controller");
 angular.module('app.search', [])
+  
   .controller('SearchController', ["$scope", "$location", "UserSearch", function($scope, $location, UserSearch) {
     $scope.userInput = {}
     $scope.userInputError = false;
-    $scope.results = false;
+    $scope.jobResults = false;
     $scope.jobs = [];
 
     $scope.searchNow = function(){
@@ -17,16 +18,16 @@ angular.module('app.search', [])
         //call the service method to make an API call to indeed
         UserSearch.getJobs($scope.userInput)
           .then( function(data){
-            console.log("data recieved from the server: ", data);
+            console.log("jobs data recieved from the server: ", data);
             $scope.jobs = data.data;
-            $scope.results = true;
+            $scope.jobResults = true;
           })
           //callthe service method to save stats to db
           .then (function(){
             UserSearch.saveStats($scope.userInput);
           })
           .then(function(data){
-            console.log("response recieved is: ", data);
+            console.log("save stats response recieved is: ", data);
           })
       }
     }
